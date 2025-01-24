66971c45d5374b8508624d555033f0943c959e4f
João Pedro Delduck <jpdelduck@gmail.com>
2025-01-24 10:45:13 -0300

    Move games on list

---
+++ src/main/java/com/github/delduck/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,13 @@
+package com.github.delduck.dslist.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReplacementDTO {
+
+	private Integer sourceIndex;
+	private Integer destinationIndex;
+	
+}
